6bffded094d6be73ae0bc853e1c6b751ea837151
Nikita <shiryaevv1@yandex.by>
2019-11-11 23:54:59 +0300

    install postgresql

--- REST_Library/settings.py
+++ REST_Library/settings.py
@@ -81,12 +81,14 @@ WSGI_APPLICATION = 'REST_Library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'library_db',
+        'USER': 'admin',
+        'PASSWORD': '123123qweqwe',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
