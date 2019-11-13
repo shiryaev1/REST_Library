067fcf19dfa67ca21ccd7bc7567e1deb36afbcc5
Nikita <shiryaevv1@yandex.by>
2019-11-14 01:13:11 +0300

    edit testcase

--- main/tests.py
+++ main/tests.py
@@ -6,35 +6,39 @@ from main.models import Book
 
 
 class AnimalTestCase(TestCase):
-    def setUp(self):
-        User.objects.create(
+    def test_setUp(self):
+        user1 = User.objects.create(
             username='user1',
             first_name='TestName',
             last_name='TestSurname',
             password='123123'
         )
-        User.objects.create(
+        user2 = User.objects.create(
             username='trainer',
             first_name='OtherName',
             last_name='OtherSurname',
             password='qweqwe'
         )
-        user1 = User.objects.get(id=1)
-        user2 = User.objects.get(id=2)
-        Book.objects.create(
+        return user1, user2
+
+    def test_book_create(self):
+        user1, user2 = self.test_setUp()
+        book1 = Book.objects.create(
             title="Learning Python",
             content="Get a comprehensive, in-depth introduction to the core Python language",
             author="Mark Lutz",
             owner=user1,
         )
-        Book.objects.create(
+        book2 = Book.objects.create(
             title="Two Scoops of Django",
             content="Two Scoops of Django: Best Practices For Django",
+            author="Lutz",
             owner=user2,
         )
 
+        return book1, book2
+
     def test_book_absolute_url(self):
-        book1 = Book.objects.get(id=1)
-        book2 = Book.objects.get(id=2)
-        self.assertEqual(book1.get_absolute_url(), '/book/1/edit/')
-        self.assertEqual(book2.get_absolute_url(), '/book/2/edit/')
+        book1, book2 = self.test_book_create()
+        self.assertEqual(book1.get_absolute_url(), f'/book/{book1.id}/edit/')
+        self.assertEqual(book2.get_absolute_url(), f'/book/{book2.id}/edit/')
